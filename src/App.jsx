f744f5fa98ec31ae75ba53a0ac2db6da1383bbfb
proteanmetamorphoses <steve.kurowski@gmail.com>
2023-11-18 22:29:00 -0700

    Stop, start, reset buttons added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import './App.css'
 
 function App() {
   const [counter, setCount] = useState(0);
+  const [running, setRunning] = useState(true);
 
   useEffect(() => {
     console.log("useEffect starting"); 
     const interval = setInterval(() => { 
       setCount(counter + 1); 
-  }, 3000); 
+    }, 3000);
+    if(running){
+      return () => clearInterval(interval); 
+    }else{
+      clearTimeout(interval);
+    }
 
-  return () => clearInterval(interval); 
-}, [counter]); 
+  
+}, [counter, running]); 
+
 
 
   return (
@@ -20,8 +27,14 @@ function App() {
         <button className="button-1" onClick={() => setCount((counter) => counter + 1)}>
           count is {counter}
         </button>
-        <button className="button-1" onClick={() => setCount(() => 0)}>
+        <button className="resetButton-1" onClick={() => setCount(() => 0)}>
           Reset
+        </button>
+        <button className="stopButton-1" onClick={() => setRunning(() => false)}>
+          Stop
+        </button>
+        <button className="startButton-1" onClick={() => setRunning(() => true)}>
+          Start
         </button>
       </div>
     </>
