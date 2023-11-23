e0f2365c479c55937670e507abd2aaf74cb60feb
proteanmetamorphoses <steve.kurowski@gmail.com>
2023-11-22 19:36:35 -0700

    Adjusted useEffect function

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,54 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [counter, setCount] = useState(0);
   const [running, setRunning] = useState(true);
 
   useEffect(() => {
-    console.log("useEffect starting"); 
-    const interval = setInterval(() => { 
-      setCount(counter + 1); 
+    console.log("useEffect starting");
+    const interval = setInterval(() => {
+      setCount(counter + 1);
     }, 3000);
-    if(running){
-      return () => clearInterval(interval); 
-    }else{
+    if (running) {
+      return () => clearInterval(interval);
+    } else {
       clearTimeout(interval);
     }
+  }, [counter, running]);
 
-  
-}, [counter, running]); 
-
-function resetCounter() {
-  setCount(0);
-  setRunning(true);
-}
-
+  function resetCounter() {
+    setCount(0);
+    setRunning(true);
+  }
 
   return (
     <>
       <div className="container">
-        <button className="button-1" onClick={() => setCount((counter) => counter + 1)}>
+        <button
+          className="button-1"
+          onClick={() => setCount((counter) => counter + 1)}
+        >
           count is {counter}
         </button>
-        <button className="startButton-1" onClick={() => setRunning(() => true)}>
+        <button
+          className="startButton-1"
+          onClick={() => setRunning(() => true)}
+        >
           Start
         </button>
-        <button className="stopButton-1" onClick={() => setRunning(() => false)}>
+        <button
+          className="stopButton-1"
+          onClick={() => setRunning(() => false)}
+        >
           Stop
         </button>
-        <button className="resetButton-1" onClick={ resetCounter }>
+        <button className="resetButton-1" onClick={resetCounter}>
           Reset
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
